7db146b7db445e9d12ebf5a60302be63557f158d
Viniciux7 <vini.15xd@gmail.com>
2026-03-30 19:34:43 -0300

    criando schemas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,19 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='Minha API BALA!')
+
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
