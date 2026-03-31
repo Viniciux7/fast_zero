a3eec2c27f14920a0aa158915306a4a40d6ad1e6
Viniciux7 <vini.15xd@gmail.com>
2026-03-30 21:01:44 -0300

    put adicionado

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI(title='Minha API BALA!')
 
@@ -17,13 +17,30 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        username=user.username,
-        email=user.email,
-        password=user.password,
-        id=len(database) + 1
-    )
-    
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
     database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_user():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{userid}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Deu ruim! Não achei...'
+        )
     
+    database[user_id - 1] = user_with_id
+
     return user_with_id
